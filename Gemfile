75d62059f842a0e60cbe05b4543c5a82439b3a38
Álvaro Casado Coscollá <alvarocasadoc@gmail.com>
2017-12-26 15:49:17 +0100

    Added Google Oauth2

--- Gemfile
+++ Gemfile
@@ -43,6 +43,7 @@ gem 'omniauth-github'
 gem 'omniauth-twitter'
 gem 'omniauth-facebook'
 gem 'omniauth-linkedin'
+gem 'omniauth-google-oauth2'
 
 gem "awesome_print", require:"ap"
 
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -30,4 +30,5 @@ Devise.setup do |config|
 
   config.omniauth :github, ENV['INFUSION_GH_ID'], ENV['INFUSION_GH_SECRET'], scope: 'user:email'
   config.omniauth :twitter,  ENV['INFUSION_TW_ID'], ENV['INFUSION_TW_SECRET']
+  config.omniauth :google_oauth2,  ENV['INFUSION_G_ID'], ENV['INFUSION_G_SECRET']
 end
