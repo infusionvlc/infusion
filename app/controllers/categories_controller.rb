b029201f6879724043aa29e4206c1ab269e8c2e7
duxy1996 <durancdr96@gmail.com>
2017-12-28 00:46:21 +0100

    fixed style

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,26 +1,33 @@
 class CategoriesController < ApplicationController
+
   def show
     @category = Category.find(params[:id])
   end
+
   def new
     @category = Category.new
   end 
+
   def create
     @category = Category.new(category_params)   
     @category.save
     redirect_to @category
   end  
+
   def index
     @categories = Category.all
   end
+
   def edit
     @category = Category.find(params[:id])
   end
+
   def destroy
     @category = Category.find(params[:id])
     @category.destroy 
     redirect_to categories_path
   end
+
   def update
     @category = Category.find(params[:id])
    
