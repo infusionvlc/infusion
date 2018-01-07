4b82e82b825f1e26c529dede00caa935f5776de8
Álvaro Casado Coscollá <alvarocasadoc@gmail.com>
2018-01-07 22:10:20 +0100

    Translated devise error messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,24 +10,24 @@ class SessionsController < ApplicationController
 
     if current_user
       if @identity.user == current_user
-        redirect_to root_url, notice: "Already linked that account!"
+        redirect_to root_url, notice: "#{I18n.t 'user.already_linked'}"
       else
         @identity.user = current_user
         @identity.save
-        redirect_to root_url, notice: "Successfully linked that account!"
+        redirect_to root_url, notice: "#{I18n.t 'user.linked'}"
       end
     else
       if @identity.user.present?
         sign_in @identity.user
-        redirect_to root_url, notice: "Signed in!"
+        redirect_to root_url, notice: "#{I18n.t 'user.logged_in'}"
       else
         if User.find_by_email(auth[:info][:email])
-          redirect_to root_url, flash: { error: "Another user account exists with your associated email! Please log in to link your social account" }
+          redirect_to root_url, flash: { error: "#{I18n.t 'user.error_sign_up'}" }
         else
           @identity.user = User.create_with_omniauth(auth)
           @identity.save
           sign_in @identity.user
-          redirect_to root_url, notice: "Welcome!"
+          redirect_to root_url, notice: "#{I18n.t 'user.logged_in'}"
         end
       end
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,11 @@
-Role.create([{name: 'Admin'}, {name: 'Plain'}])
+Role.create([
+        {name: 'Admin'}, {name: 'Plain'}
+    ])
+Category.create([
+        {name: 'VR'}, {name: 'AR'},
+        {name: 'XR'}, {name: 'Videojuegos'},
+        {name: '3D'}, {name: 'CV'},
+        {name: 'Web'}, {name: 'Accesibilidad'},
+        {name: 'Móvil'}, {name: 'Vídeo'},
+        {name: 'Imagen'}, {name: 'Sonido'}
+    ])
