2e29f96740ae9f571d4c9520c54a44b53270f866
Álvaro Casado Coscollá <alvarocasadoc@gmail.com>
2018-01-17 19:55:55 +0100

    Added locale field on profile update page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180110112149) do
+ActiveRecord::Schema.define(version: 20180112130352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 20180110112149) do
     t.time "end"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "confirmation_mail"
   end
 
   create_table "photos", force: :cascade do |t|
