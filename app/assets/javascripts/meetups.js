58eda872e035f54d636a8dc313d3220615b0115b
David <pitu96_drn23@hotmail.com>
2018-01-22 20:18:11 +0100

    Implemented Ajax review

--- app/assets/javascripts/meetups.js
+++ app/assets/javascripts/meetups.js
@@ -1,29 +1,8 @@
 $(document).on('turbolinks:load', function(){
-    $('.edit_comment').click(function(){
-        var arr = [
-            {val : 1, text: '1'},
-            {val : 2, text: '2'},
-            {val : 3, text: '3'},
-            {val : 3, text: '4'},
-            {val : 3, text: '5'}
-          ];
-
-        var comment = $('.edit_comment ~ .card-text');
-        var data = comment.html();
-        comment.empty();
-        $form = $("<form></form>");
-        var text = document.createElement('input');
-        text.setAttribute('type', 'text');
-        text.setAttribute('value', data);
-        $form.append(text);
-        comment.append($form);
-        var star = $('.edit_comment ~ .card-title');
-        star.empty();
-        var sel = $('<select>').appendTo(star);
-        $(arr).each(function() {
-         sel.append($("<option>").attr('value',this.val).text(this.text));
-        });
+    $('.edit_comment').click(function(e){
+        var id = $(this).attr("id").split("_")[2];
+        $('#info_'+id).hide();
+        $('#edit_form_'+id).show();
         return false;
-    }
-);
+    });
 });
--- app/models/assistance.rb
+++ app/models/assistance.rb
@@ -1,4 +1,10 @@
 class Assistance < ApplicationRecord
   belongs_to :user
   belongs_to :meetup
+  validates  :review, length: {minimum: 5}, if: :should_validate?
+  validates  :mark, numericality: {greater_than: -1, less_than_or_equal_to: 5}, if: :should_validate?
+
+  def should_validate?
+    not new_record?
+  end
 end
