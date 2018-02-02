1159cb332d39c46bb88882f58b56e7f533a85773
Josep Dols <jodoldar@gmail.com>
2018-02-02 16:58:54 +0100

    Test corregidos

--- test/models/photo_test.rb
+++ test/models/photo_test.rb
@@ -23,9 +23,4 @@ class PhotoTest < ActiveSupport::TestCase
 		puts "\nTest: Photo -> Invalid without meetup"
 	end
 
-	test 'Invalid without attribution' do
-		photo = Photo.new(title: "TestTitle1", meetup_id: meetup.id)
-		assert_not_equal photo.errors.count, 0, 'Photo is valid without an Attribution'
-		puts "\nTest: Photo -> Invalid without attribution"
-	end
 end
