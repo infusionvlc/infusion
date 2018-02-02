51941f583529dbfbca31a90ca2aeed9fbb5e700b
mko010 <miguel_estevan@hotmail.com>
2018-02-02 18:04:36 +0100

    Test location

--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
 class LocationTest < ActiveSupport::TestCase
-
   test 'Invalid without name' do
-    location = Location.new()
+    location = Location.new
     refute location.valid?, 'Location is valid without a Name'
     puts "\nTest: Location -> Invalid without name"
   end
