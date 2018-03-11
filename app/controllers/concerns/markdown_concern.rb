3b07aef8e37e088a1566ffbdd3b5d3714c10fed1
Álvaro Casado Coscollá <alvarocasadoc@gmail.com>
2018-03-11 18:40:19 +0100

    Added markdown editor and viewer

---
+++ app/controllers/concerns/markdown_concern.rb
@@ -0,0 +1,11 @@
+module MarkdownConcern
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :set_renderer, only: %i[show edit new]
+  end
+
+  def set_renderer
+    @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new({link_attributes: {target: '_blank'}}), autolink: true, tables: true, hard_wrap: true)
+  end
+end
