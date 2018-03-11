21e294f32b89ef55676637e6ca7b4fe3f4a30c56
Álvaro Casado Coscollá <alvarocasadoc@gmail.com>
2018-03-11 21:34:01 +0100

    Fixed md visualization issues

--- app/controllers/concerns/markdown_concern.rb
+++ app/controllers/concerns/markdown_concern.rb
@@ -2,7 +2,7 @@ module MarkdownConcern
   extend ActiveSupport::Concern
 
   included do
-    before_action :set_renderer, only: %i[show edit new]
+    before_action :set_renderer, only: %i[show edit new index call_for_talks archive]
   end
 
   def set_renderer
