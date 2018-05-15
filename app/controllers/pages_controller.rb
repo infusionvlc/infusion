1378b5a8fc18eb3fcf6e91e90087f412c54327fc
Álvaro Casado Coscollá <alvarocasadoc@gmail.com>
2018-05-15 13:44:30 +0200

    Added show & tell speakers page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,6 @@ class PagesController < ApplicationController
   def index; end
 
   def rules; end
+
+  def speakers; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   match '/rules' => 'pages#rules', via: :get
   match '/reglas' => 'pages#reglas', via: :get
   match '/regles' => 'pages#regles', via: :get
+  match '/speakers' => 'pages#speakers', via: :get
 
   match '/read_notifications',           to: 'notifications#mark_all_as_read', via: [:post]
 
