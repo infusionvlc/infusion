ed7a5cd677145ad0fadb8fb1a82f1acd4d8c952e
Álvaro Casado Coscollá <alvarocasadoc@users.noreply.github.com>
2018-09-14 18:39:00 +0200

    Update 20180908124009_rename_meetup_id_in_assistances.rb

--- db/migrate/20180908124009_rename_meetup_id_in_assistances.rb
+++ db/migrate/20180908124009_rename_meetup_id_in_assistances.rb
@@ -1,7 +1,5 @@
 class RenameMeetupIdInAssistances < ActiveRecord::Migration[5.1]
   def change
-    remove_foreign_key :assistances, :meetups
     rename_column      :assistances, :meetup_id, :session_id
-    add_foreign_key    :assistances, :sessions
   end
 end
