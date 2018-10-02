6f79e9b75f1320ae5c9b4f556ee0a9ff8aa8c6ba
Manuel Lorenzo Parejo <manulorenzop@gmail.com>
2018-10-02 14:15:52 +0200

    removed problematic test, and using now routes paths helpers

--- test/controllers/meetups_controller_test.rb
+++ test/controllers/meetups_controller_test.rb
@@ -9,28 +9,24 @@ class MeetupsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should redirect to meetup page if there is not id in params' do
-    get '/meetups/new'
-    assert_response :redirect
-    follow_redirect!
-    assert_response :success
+    get new_meetup_path
+    assert_redirected_to meetups_path
   end
 
   test 'should redirect to main page if the id in params is not valid' do
-    get '/meetups/1'
-    assert_response :redirect
-    follow_redirect!
-    assert_response :success
+    get meetup_path(1)
+    assert_redirected_to meetups_path
   end
 
   test 'should create new meetup with title and description' do
-    post '/meetups', params: { meetup: { title: 'title', description: 'description' } }
+    post meetups_path, params: { meetup: { title: 'title', description: 'description' } }
     assert_response :redirect
     follow_redirect!
     assert_response :success
   end
 
-  test 'should respond with an error when trying to create new meetup with no params' do
-    post '/meetups', params: { meetup: {  } }
-    assert_response :error
+  test 'should update existing meetup and redirect to meetup#index' do
+    patch meetup_path(Meetup.first), params: { meetup: { title: 'my new title' } }
+    assert_redirected_to meetups_path
   end
 end
