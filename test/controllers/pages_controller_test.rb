d5d6faff52e284483efc1984ac76634915378724
Manuel Lorenzo Parejo <manulorenzop@gmail.com>
2018-10-02 14:26:23 +0200

    added pages controller tests for static pages

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'get about page should show the page' do
+    get '/about'
+    assert_response :success
+  end
+
+  test 'get rules page should show the page' do
+    get '/rules'
+    assert_response :success
+  end
+
+  test 'get reglas page should show the page' do
+    get '/reglas'
+    assert_response :success
+  end
+
+  test 'get regles page should show the page' do
+    get '/regles'
+    assert_response :success
+  end
+
+  test 'get hacktoberfest page should show the page' do
+    get '/hacktoberfest'
+    assert_response :success
+  end
 end
