ac6d81021f762f42a1b4456c2507b5dca21aa017
Manuel Lorenzo <m.lorenzoparejo@minddistrict.com>
2018-10-02 12:19:52 +0200

    added first meetup controller test

--- test/controllers/meetups_controller_test.rb
+++ test/controllers/meetups_controller_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class MeetupsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should show main page' do
+    get '/meetups/index'
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+  end
 end
