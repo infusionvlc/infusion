d4a50f2679b149528d6eb4e3096e7367ae4d4c22
Manuel Lorenzo Parejo <manulorenzop@gmail.com>
2018-10-07 14:12:51 +0200

    added tests for meetup_controller#edit, #destroy and #show

--- test/controllers/meetups_controller_test.rb
+++ test/controllers/meetups_controller_test.rb
@@ -3,6 +3,8 @@
 require 'test_helper'
 
 class MeetupsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   test 'should show main page' do
     get '/meetups/index'
     assert_response :redirect
@@ -32,5 +34,26 @@ class MeetupsControllerTest < ActionDispatch::IntegrationTest
     patch meetup_path(Meetup.first),
           params: { meetup: { title: 'my new title' } }
     assert_redirected_to meetups_path
+  end
+
+  test 'should show a meetup' do
+    sign_in users(:one)
+    get meetup_path(1)
+    follow_redirect!
+    assert_response :success
+  end
+
+  test 'should remove a meetup' do
+    sign_in users(:one)
+    delete meetup_path(1)
+    follow_redirect!
+    assert_response :success
+  end
+
+  test 'should edit a meetup' do
+    sign_in users(:one)
+    get edit_meetup_path(1)
+    follow_redirect!
+    assert_response :success
   end
 end
