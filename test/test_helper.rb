16652a36485ed25033e46751880e7cff9e2dffcd
Manuel Lorenzo Parejo <manulorenzop@gmail.com>
2018-10-11 17:59:39 +0200

    fixing rubocop

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -23,16 +23,18 @@ end
 require 'capybara/rails'
 require 'capybara/minitest'
 
-class ActionDispatch::IntegrationTest
-  # Make the Capybara DSL available in all integration tests
-  include Capybara::DSL
-  # Make `assert_*` methods behave like Minitest assertions
-  include Capybara::Minitest::Assertions
+module ActionDispatch
+    class IntegrationTest
+    # Make the Capybara DSL available in all integration tests
+    include Capybara::DSL
+    # Make `assert_*` methods behave like Minitest assertions
+    include Capybara::Minitest::Assertions
 
-  # Reset sessions and driver between tests
-  # Use super wherever this method is redefined in your individual test classes
-  def teardown
-    Capybara.reset_sessions!
-    Capybara.use_default_driver
+    # Reset sessions and driver between tests
+    # Use super wherever this method is redefined in your individual test classes
+    def teardown
+      Capybara.reset_sessions!
+      Capybara.use_default_driver
+    end
   end
 end
