228cdff21bfe8710376b560bdeca2751ceb39e48
Manuel Lorenzo Parejo <manulorenzop@gmail.com>
2018-10-11 17:52:27 +0200

    added integration tests with capybara for the static pages

---
+++ test/integration/pages_flow_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class PagesFlowTest < ActionDispatch::IntegrationTest
+  test 'about page' do
+    visit root_path
+    click_link 'About'
+    assert_current_path about_path
+  end
+
+  test 'hacktoberfest page' do
+    visit hacktoberfest_path
+    assert_current_path hacktoberfest_path
+  end
+
+  test 'rules page' do
+    visit rules_path
+    assert_current_path rules_path
+  end
+
+  test 'reglas page' do
+    visit reglas_path
+    assert_current_path reglas_path
+  end
+  
+  test 'regles page' do
+    visit regles_path
+    assert_current_path regles_path
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -19,3 +19,20 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
+require 'capybara/rails'
+require 'capybara/minitest'
+
+class ActionDispatch::IntegrationTest
+  # Make the Capybara DSL available in all integration tests
+  include Capybara::DSL
+  # Make `assert_*` methods behave like Minitest assertions
+  include Capybara::Minitest::Assertions
+
+  # Reset sessions and driver between tests
+  # Use super wherever this method is redefined in your individual test classes
+  def teardown
+    Capybara.reset_sessions!
+    Capybara.use_default_driver
+  end
+end
