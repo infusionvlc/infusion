7e3defec8c7ac546678f347e1cc5a6849f794fce
Manuel Lorenzo Parejo <manulorenzop@gmail.com>
2018-10-20 18:07:45 +0200

    added tests to sessions

--- test/models/session_test.rb
+++ test/models/session_test.rb
@@ -3,7 +3,27 @@
 require 'test_helper'
 
 class SessionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'a session is invalid without any event_id, location_id, meetup_id' do
+    session = Session.new
+    assert_not session.valid?
+  end
+
+  test 'valid session' do
+    event = events(:one)
+    location = locations(:one)
+    meetup = meetups(:one)
+
+    session = Session.new(event_id: event.id, location_id: location.id, meetup_id: meetup.id)
+    assert session.valid?, true
+  end
+
+  test 'valid session has an average rating' do
+    event = events(:one)
+    location = locations(:one)
+    meetup = meetups(:one)
+
+    session = Session.new(event_id: event.id, location_id: location.id, meetup_id: meetup.id)
+
+    assert_equal session.average_rating, 0
+  end
 end
