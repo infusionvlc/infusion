697f363145920c2fd80369102fe0a460c8dcd245
Manuel Lorenzo Parejo <manulorenzop@gmail.com>
2018-10-20 17:12:15 +0200

    added event test

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -3,7 +3,18 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'two events cannot have the same date' do
+    now = Time.now
+
+    event = Event.new(date: now)
+    assert_nothing_raised do
+      event.save!
+    end
+
+    event1 = Event.new(date: now)
+    event.save!
+    assert_not_nil event.errors
+
+    puts 'Test: Event -> Imposible to have two events with the same date'
+  end
 end
