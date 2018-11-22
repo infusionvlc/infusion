02e877320f3dc8e5db7a950505578b24f3938647
Manuel Lorenzo Parejo <manulorenzop@gmail.com>
2018-11-22 22:13:36 +0100

    replaced several tests for one that tests all cases

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -4,26 +4,6 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  test 'Valid Report with type hate' do
-    test_report_type(0)
-  end
-
-  test 'Valid Report with type menace' do
-    test_report_type(1)
-  end
-
-  test 'Valid Report with type pornography' do
-    test_report_type(2)
-  end
-
-  test 'Valid Report with type community' do
-    test_report_type(3)
-  end
-
-  test 'Valid Report with type other' do
-    test_report_type(4)
-  end
-
   test 'Invalid without title' do
     user = users(:one)
     meetup = meetups(:one)
@@ -112,47 +92,26 @@ class ReportTest < ActiveSupport::TestCase
     puts 'Test: Report -> Invalid without description'
   end
 
-  private
-
-  def test_report_type(report_type)
+  test 'Report with a valid type' do
     user = users(:one)
     meetup = meetups(:one)
-    report = Report.new(title: 'TestTitle',
-                        type_of: report_type,
+
+    types = {0 => 'Incitación al odio', 
+             1 => 'Amenazas o insultos', 
+             2 => 'Pornografía', 
+             3 => 'No respeta las normas de la comunidad', 
+             4 => 'Otro'}
+    types.each do |key, value|
+      report = Report.new(title: 'TestTitle',
+                        type_of: key,
                         reportable_type: 'Meetup',
                         user_id: user.id,
                         reportable_id: meetup.id,
                         description: 'TestDescritpion')
-    if report_type == 0
       assert_equal report.valid?, true, 'Proposal is invalid with all info'
-      assert_equal report.text_type, 'Incitación al odio'
+      assert_equal report.text_type, value
       assert_equal report.text_status, 'Abierta'
       assert_equal report.reportable_title, meetup.title
-      puts 'Test: Report -> Valid Report with type hate'
-    elsif report_type == 1
-      assert_equal report.valid?, true, 'Proposal is invalid with all info'
-      assert_equal report.text_type, 'Amenazas o insultos'
-      assert_equal report.text_status, 'Abierta'
-      assert_equal report.reportable_title, meetup.title
-      puts 'Test: Report -> Valid Report with type menace'
-    elsif report_type == 2
-      assert_equal report.valid?, true, 'Proposal is invalid with all info'
-      assert_equal report.text_type, 'Pornografía'
-      assert_equal report.text_status, 'Abierta'
-      assert_equal report.reportable_title, meetup.title
-      puts 'Test: Report -> Valid Report with type pornography'
-    elsif report_type == 3
-      assert_equal report.valid?, true, 'Proposal is invalid with all info'
-      assert_equal report.text_type, 'No respeta las normas de la comunidad'
-      assert_equal report.text_status, 'Abierta'
-      assert_equal report.reportable_title, meetup.title
-      puts 'Test: Report -> Valid Report with type community'
-    elsif report_type == 4
-      assert_equal report.valid?, true, 'Proposal is invalid with all info'
-      assert_equal report.text_type, 'Otro'
-      assert_equal report.text_status, 'Abierta'
-      assert_equal report.reportable_title, meetup.title
-      puts 'Test: Report -> Valid Report with type other'
     end
   end
 end
